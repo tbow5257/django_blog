dad18793a3df3f40f816f4f630a2d50783b7a1fc
dankbytes <1337tjb@gmail.com>
2017-05-10 21:44:15 -0700

    adding admin posts module tutorial.p8

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "timestamp"]
+    class Meta:
+        model = Post
 
-admin.site.register(Post)
+
+admin.site.register(Post, PostModelAdmin)
 
